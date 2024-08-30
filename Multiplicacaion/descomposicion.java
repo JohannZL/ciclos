12732981bcb6f3c28b78a9a0afa7f5dc6656d128
unknown <jodzambranol@udistrital.edu.co>
2024-08-29 21:47:44 -0500

    ciclos

--- Multiplicacaion/descomposicion.java
+++ Multiplicacaion/descomposicion.java
@@ -5,10 +5,11 @@ class descomposicion
     public static void main(String[] args)
     {
         Scanner leer = new Scanner(System.in);
-        int a,b,c,d=0;
+        int a,b,c,d,e;
         System.out.println("ingrese un numero: ");
         a = leer.nextInt();
         c = a;
+        d=0;
         if(a<10)
         {
             System.out.println("se descomposicion es: "+a);
@@ -17,8 +18,13 @@ class descomposicion
             while(c>=10)
             {
                 b=c%10;
-                System.out.println("sus datos son: "+b);
                 c=c/10;
+                e=10^d;
+                System.out.println("sus datos son: "+b);
+                if(c<10)
+                {
+                    System.out.println("sus datos son: "+c);
+                }
                 d++;
             }
         }
